ca50336990a4660b17a102dbd3ac1ce43971ce52
sh15fa <shahd152000@hotmail.com>
2023-09-23 20:18:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+export default function App() {
+const [name, setName] = useState(0);
+const [mobile, setMobile] = useState("");
+const [darkMode, setDarkMode] = useState(false);
+// let dependency = { name, mobile };==>wrong solution 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//the dependency must be variables or states that put in the array of dependency in useEffect not object.
+useEffect(() => {
+console.log("UseEffect is called");
+}, [ name, mobile ]);//==>right solution
+return (
+    <div className={`container ${darkMode ? "bgDark" : "bgLight"}`}>
+    <input
+    type="text"
+    id="name"
+    placeholder="Name"
+    onChange={(e) => setName(e.target.value)}
+    />
+    <input
+    type="text"id="mobile"
+    placeholder="Mobile"
+    onChange={(e) => setMobile(e.target.value)}
+    />
+    <div className="darkmode">
+    <input
+    type="checkbox"
+    id="darkMode"
+    onChange={() => setDarkMode(!darkMode)}
+    />
+    Enable dark mode
     </div>
-  );
+    </div>
+    );
 }
-
-export default App;
